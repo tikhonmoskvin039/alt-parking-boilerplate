98a5c9994d49f5d6296665d2d4efe8deeb24a5c5
Moskvin Tikhon S. <tihonmoskvin@mail.ru>
2025-11-26 16:18:24 +0300

    feat: add  custom github actions + your eslint config guard on any PR

--- eslint.config.js
+++ eslint.config.js
@@ -6,13 +6,19 @@ import importPlugin from "eslint-plugin-import";
 import tseslint from "typescript-eslint";
 import prettierPlugin from "eslint-plugin-prettier";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      // ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
       importPlugin.flatConfigs.recommended,
       importPlugin.flatConfigs.typescript,
       eslintConfigPrettier,
@@ -21,6 +27,10 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: resolve(__dirname), // абсолютный путь к корню
+        project: "./tsconfig.app.json", // твой tsconfig для фронтенда
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
@@ -34,6 +44,18 @@ export default tseslint.config(
       "prettier/prettier": "error",
       "import/default": "off",
       "import/named": "off",
+      // только нужное правило включаем строго
+      "@typescript-eslint/restrict-plus-operands": "error",
+
+      // остальные типизированные правила отключаем
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-misused-promises": "off",
+      "@typescript-eslint/no-floating-promises": "off",
+      "@typescript-eslint/no-redundant-type-constituents": "off",
       "import/order": [
         "error",
         {
